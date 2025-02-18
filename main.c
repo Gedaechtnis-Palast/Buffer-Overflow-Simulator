e8dbe3534868570189840b40b4a14521fb382e06
Lenni-Evens <mayeranna777@gmail.com>
2025-02-18 13:22:05 +0100

    auslesen & bearbeiten der test.txt

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include "loopiterator.h"
+#include <stdlib.h>
+#define STR_LEN 80
 
 int main(){
-    loopIterator();
-
-    return 0;
+    // double array[10];
+    // printf("%p", array);
+    FILE *fp;
+    char *filename = "test.txt";
+    // Datei oeffnen und eine Zeile anhaengen
+    fp = fopen (filename, "a");
+    if (fp == NULL)
+    {
+        // Fehlerbehandlung
+        fprintf (stderr, "Fehler beim Oeffnen der Datei '%s'\n",
+        filename);
+        return EXIT_FAILURE;
+    }
+    fprintf(fp, "\nNoch eine Zeile anhaengen ...");
+    // Datei schliessen
+    fclose(fp);
+    if ((fp = fopen (filename, "r")) == NULL)
+    {
+    // Fehlerbehandlung
+        fprintf(stderr, "Fehler beim Öffnen der Datei '%s'\n",
+        filename);      
+        return EXIT_FAILURE;
+    }
+    char str[STR_LEN];
+    while(fscanf(fp, "%s", str) != EOF) 
+        printf ("%s", str);
+    // Datei schliessen
+    fclose (fp);
+    return EXIT_SUCCESS;
 }

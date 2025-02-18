302ebbeda63e6f1080bafc3f27f28c09623d6d2b
Lenni-Evens <mayeranna777@gmail.com>
2025-02-18 13:22:05 +0100

    auslesen & bearbeiten der test.txt

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define STR_LEN 80
 
 int main(){
-    printf("Hallo");
-    return 0;
+    // double array[10];
+    // printf("%p", array);
+    FILE *fp;
+    char *filename = "test.txt";
+    // Datei oeffnen und eine Zeile anhaengen
+    fp = fopen (filename, "a");
+    if (fp == NULL)
+    {
+        // Fehlerbehandlung
+        fprintf (stderr, "Fehler beim Oeffnen der Datei '%s'\n",
+        filename);
+        return EXIT_FAILURE;
+    }
+    fprintf(fp, "\nNoch eine Zeile anhaengen ...");
+    // Datei schliessen
+    fclose(fp);
+    if ((fp = fopen (filename, "r")) == NULL)
+    {
+    // Fehlerbehandlung
+        fprintf(stderr, "Fehler beim Öffnen der Datei '%s'\n",
+        filename);      
+        return EXIT_FAILURE;
+    }
+    char str[STR_LEN];
+    while(fscanf(fp, "%s", str) != EOF) 
+        printf ("%s", str);
+    // Datei schliessen
+    fclose (fp);
+    return EXIT_SUCCESS;
 }

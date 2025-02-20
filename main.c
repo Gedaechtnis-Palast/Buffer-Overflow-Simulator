1e5e765b3d49df8599d3a20ada47d9c56eb54dea
Lenni-Evens <mayeranna777@gmail.com>
2025-02-20 09:56:07 +0100

    finish read file &  return string

--- main.c
+++ main.c
@@ -2,34 +2,45 @@
 #include <stdlib.h>
 #define STR_LEN 80
 
-int main(){
-    // double array[10];
-    // printf("%p", array);
+char* readFile(const char* filename) {
     FILE *fp;
-    char *filename = "test.txt";
-    // Datei oeffnen und eine Zeile anhaengen
-    fp = fopen (filename, "a");
-    if (fp == NULL)
-    {
-        // Fehlerbehandlung
-        fprintf (stderr, "Fehler beim Oeffnen der Datei '%s'\n",
-        filename);
-        return EXIT_FAILURE;
+    char *buffer = NULL;
+    size_t size = 0;
+
+    // Datei öffnen
+    fp = fopen(filename, "r");
+    if (fp == NULL){
+        fprintf(stderr, "Fehler beim öffnen der Datei '%s'\n", filename);
+        return NULL; 
     }
-    fprintf(fp, "\nNoch eine Zeile anhaengen ...");
-    // Datei schliessen
+    // Dateigröße ermitteln
+    fseek(fp, 0, SEEK_END);
+    size = ftell(fp);
+    rewind(fp);
+
+    // Speicher für den inhalt der Datei reservieren
+    buffer = (char*)malloc((size + 1) + sizeof(char));
+    if (buffer == NULL){
+        fprintf(stderr, "Speicherzuweisung felgeschlagen\n");
+        fclose(fp);
+        return NULL;
+    }
+    // Dateiinhalt lesen
+    fread(buffer, sizeof(char), size, fp); // Nullterminierung
+    // Datei schließen
     fclose(fp);
-    if ((fp = fopen (filename, "r")) == NULL)
-    {
-    // Fehlerbehandlung
-        fprintf(stderr, "Fehler beim Öffnen der Datei '%s'\n",
-        filename);      
-        return EXIT_FAILURE;
+
+    return buffer;
+    
+}
+
+int main(){
+    const char *filename = "test.txt";
+    char *content = readFile(filename);
+
+    if (content != NULL){
+        printf("Dateiinhalt:\n%s\n", content);
+        free(content); // Speicher freigeben
     }
-    char str[STR_LEN];
-    while(fscanf(fp, "%s", str) != EOF) 
-        printf ("%s", str);
-    // Datei schliessen
-    fclose (fp);
     return EXIT_SUCCESS;
 }

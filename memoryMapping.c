879a3eba97cd0954f58c2d3ced1569fdedfbb41a
ChrisTopher <inf24268@lehre.dhbw-stuttgart.de>
2025-02-22 12:55:32 +0100

    quick save

--- memoryMapping.c
+++ memoryMapping.c
@@ -1,18 +1,94 @@
 #include "memoryMapping.h"
 
-int allocHeap(void *startAddress, void *endAddress, void *overflowEndAddress)
+SEGMENT_COLLECTION stack;
+SEGMENT_COLLECTION heap;
+SEGMENT_COLLECTION staticMem;
+
+SEGMENT createSegment(void *startAddress, void *endAddress, void *overflowEndAddress)
 {
-    int test = 1;
-    printf("pointer print: %p\n", &test);
-    char pointerChars[100] = (char)&test;
-    printf("pointer chars print: %s", pointerChars);
-    return 0;
+    SEGMENT segment = {0, 0, 0};
+    if (startAddress != NULL) // TODO: error handling
+    {
+        segment.startAddress = (uintptr_t)startAddress;
+    }
+    if (endAddress != NULL)
+    {
+        segment.endAddress = (uintptr_t)endAddress;
+    }
+    if (overflowEndAddress != NULL)
+    {
+        segment.overflowEndAddress = (uintptr_t)overflowEndAddress;
+    }
+    return segment;
 };
-int allocStack(void *startAddress, void *endAddress, void *overflowEndAddress)
+
+SEGMENT_COLLECTION createSegmentCollection(int initialSegmentCapacity)
 {
-    return 0;
+    SEGMENT_COLLECTION collection = {
+        (SEGMENT *)malloc(initialSegmentCapacity * sizeof(SEGMENT)), // TODO: error handling
+        {0, 0, 0},
+        0,
+        initialSegmentCapacity};
+    return collection;
 };
-int allocStatic(void *startAddress, void *endAddress, void *overflowEndAddress)
+
+bool initMemoryMap()
 {
-    return 0;
+    stack = createSegmentCollection(20);
+    heap = createSegmentCollection(20);
+    staticMem = createSegmentCollection(20);
+};
+
+void printMemoryMap() {
+    // get max and min from segment collection
+    // get each segment from collection
+    //
+};
+
+bool allocStack(void *startAddress, void *endAddress, void *overflowEndAddress, size_t dataTypeSize)
+{
+    return allocSegment(startAddress, endAddress, overflowEndAddress, dataTypeSize, stack);
+};
+bool allocHeap(void *startAddress, void *endAddress, void *overflowEndAddress, size_t dataTypeSize)
+{
+    return allocSegment(startAddress, endAddress, overflowEndAddress, dataTypeSize, heap);
+};
+bool allocStatic(void *startAddress, void *endAddress, void *overflowEndAddress, size_t dataTypeSize)
+{
+    return allocSegment(startAddress, endAddress, overflowEndAddress, dataTypeSize, staticMem);
+};
+
+bool allocSegment(void *startAddress, void *endAddress, void *overflowEndAddress, size_t dataTypeSize, SEGMENT_COLLECTION collection)
+{
+    SEGMENT newSegment = createSegment(startAddress, endAddress, overflowEndAddress);
+    if (newSegment.startAddress != NULL)
+    {
+        if (collection.finalSize.startAddress > newSegment.startAddress)
+        {
+            collection.finalSize.startAddress = newSegment.startAddress;
+        }
+        else if (collection.finalSize.endAddress < newSegment.startAddress)
+        {
+            collection.finalSize.endAddress = newSegment.startAddress + dataTypeSize;
+        }
+    }
+    else
+    {
+        return false;
+    }
+    if (newSegment.endAddress != NULL)
+    {
+        if (collection.finalSize.endAddress < newSegment.endAddress)
+        {
+            collection.finalSize.endAddress = newSegment.endAddress + dataTypeSize;
+        }
+    }
+    if (newSegment.overflowEndAddress != NULL)
+    {
+        if (collection.finalSize.endAddress < newSegment.overflowEndAddress)
+        {
+            collection.finalSize.endAddress = newSegment.overflowEndAddress + dataTypeSize;
+        }
+    }
+    return true;
 };
--- memoryMapping.h
+++ memoryMapping.h
@@ -1,9 +1,36 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
 typedef struct
 {
-    int startAddress;
-    int endAddress;
-    int overflowEndAddress
+    unsigned long startAddress;
+    unsigned long endAddress;
+    unsigned long overflowEndAddress;
 } SEGMENT;
+
+typedef struct
+{
+    SEGMENT array;
+    SEGMENT finalSize;
+    int mappedSegments;
+    int segmentCapacity;
+
+} SEGMENT_COLLECTION;
+
+/**
+ * Creates a SEGMENT, which represents data stored in the memory.
+ */
+SEGMENT createSegment(void *startAddress, void *endAddress, void *overflowEndAddress);
+/**
+ * Creates a SEGMENT_COLLECTION, which represents either the heap, stack or static areas in memory.
+ */
+SEGMENT_COLLECTION createSegmentCollection(int initialSegmentCapacity);
+/**
+ * Initialization of internal memory mapping for a console visualization.
+ */
+bool initMemoryMap();
+void printMemoryMap();
 
 /**
  * Accepts the start and end address of a variable to map for the memory visualization.
@@ -12,7 +39,7 @@ typedef struct
  * @endAddress the pointer to the last allocated cell of an array. (optional)
  * @overflowEndAddress the address indicating the reach of the overflow over the endAddress. (optional)
  */
-int allocHeap(void *startAddress, void *endAddress, void *overflowEndAddress);
+bool allocStack(void *startAddress, void *endAddress, void *overflowEndAddress, size_t dataTypeSize);
 /**
  * Accepts the start and end address of a variable to map for the memory visualization.
  * Optionally an extra address can be passed to indicate an overflow reaching over the end address.
@@ -20,7 +47,7 @@ int allocHeap(void *startAddress, void *endAddress, void *overflowEndAddress);
  * @endAddress the pointer to the last allocated cell of an array. (optional)
  * @overflowEndAddress the address indicating the reach of the overflow over the endAddress. (optional)
  */
-int allocStack(void *startAddress, void *endAddress, void *overflowEndAddress);
+bool allocHeap(void *startAddress, void *endAddress, void *overflowEndAddress, size_t dataTypeSize);
 /**
  * Accepts the start and end address of a variable to map for the memory visualization.
  * Optionally an extra address can be passed to indicate an overflow reaching over the end address.
@@ -28,4 +55,6 @@ int allocStack(void *startAddress, void *endAddress, void *overflowEndAddress);
  * @endAddress the pointer to the last allocated cell of an array. (optional)
  * @overflowEndAddress the address indicating the reach of the overflow over the endAddress. (optional)
  */
-int allocStatic(void *startAddress, void *endAddress, void *overflowEndAddress);
+bool allocStatic(void *startAddress, void *endAddress, void *overflowEndAddress, size_t dataTypeSize);
+
+bool allocSegment(void *startAddress, void *endAddress, void *overflowEndAddress, size_t dataTypeSize, SEGMENT_COLLECTION collection);

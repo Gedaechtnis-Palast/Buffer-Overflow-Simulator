028cdbd071f3ee25d8d04e10f519fd25cec5c916
ChrisTopher <inf24268@lehre.dhbw-stuttgart.de>
2025-02-23 21:15:59 +0100

    add terminal flags

--- fileinput.h
+++ fileinput.h
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
 #define STR_LEN 256 // Maximale Länge des Dateipfads
-char* readFile(const char* filename);
+
+char *readFile(const char *filename);
--- main.c
+++ main.c
@@ -1,33 +1,56 @@
 #include "loopiterator.h"
 #include "fileinput.h"
+#include <stdbool.h>
 
 #define FILE_PATH_FLAG "-p"
+#define AUTO_ATTACK_FLAG "--auto-attack"
+#define USE_DEP_FLAG "--use-dep"
 
 int main(int argc, char **argv)
 {
     char filename[STR_LEN];
+    bool filenameProvided = false;
+    bool automatedAttack = false;
+    bool depSecurityActive = false;
 
     for (int i = 1; i < argc; i++)
     {
         if (!strcmp(argv[i], FILE_PATH_FLAG) && i + 1 < argc)
         {
             strncpy(filename, argv[i + 1], STR_LEN);
-            printf("filename: %s\n", filename);
+            filenameProvided = true;
         }
-        printf("%s\n", argv[i]);
+        if (!strcmp(argv[i], AUTO_ATTACK_FLAG))
+        {
+            automatedAttack = true;
+        }
+        if (!strcmp(argv[i], USE_DEP_FLAG))
+        {
+            depSecurityActive = true;
+        }
     }
-    return EXIT_SUCCESS;
 
-    // Benutzereingabe für den Dateipfad
-    printf("Bitte geben Sie den Dateipfad ein: ");
-    if (fgets(filename, STR_LEN, stdin) == NULL)
+    if (automatedAttack)
     {
-        fprintf(stderr, "Fehler beim Einlesen des Dateipfads\n");
-        return EXIT_FAILURE;
+        printf("automated attack\n");
+    }
+    if (depSecurityActive)
+    {
+        printf("DEP security is active\n");
     }
 
-    // Entferne das '\n' am Ende des Eingabestrings
-    filename[strcspn(filename, "\n")] = '\0';
+    if (!filenameProvided)
+    { // Benutzereingabe für den Dateipfad
+        printf("Bitte geben Sie den Dateipfad ein: ");
+        if (fgets(filename, STR_LEN, stdin) == NULL)
+        {
+            fprintf(stderr, "Fehler beim Einlesen des Dateipfads\n");
+            return EXIT_FAILURE;
+        }
+
+        // Entferne das '\n' am Ende des Eingabestrings
+        filename[strcspn(filename, "\n")] = '\0';
+    }
 
     // Datei einlesen
     char *content = readFile(filename);

f381bfe9c710452a45b9d34904c8606d2f9cd9e5
ChrisTopher <inf24268@lehre.dhbw-stuttgart.de>
2025-02-23 15:42:40 +0100

    quick save

--- main.c
+++ main.c
@@ -1,13 +1,27 @@
 #include "loopiterator.h"
 #include "fileinput.h"
 
+#define FILE_PATH_FLAG "-p"
 
-int main() {
+int main(int argc, char **argv)
+{
     char filename[STR_LEN];
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (!strcmp(argv[i], FILE_PATH_FLAG) && i + 1 < argc)
+        {
+            strncpy(filename, argv[i + 1], STR_LEN);
+            printf("filename: %s\n", filename);
+        }
+        printf("%s\n", argv[i]);
+    }
+    return EXIT_SUCCESS;
 
     // Benutzereingabe für den Dateipfad
     printf("Bitte geben Sie den Dateipfad ein: ");
-    if (fgets(filename, STR_LEN, stdin) == NULL) {
+    if (fgets(filename, STR_LEN, stdin) == NULL)
+    {
         fprintf(stderr, "Fehler beim Einlesen des Dateipfads\n");
         return EXIT_FAILURE;
     }
@@ -17,7 +31,8 @@ int main() {
 
     // Datei einlesen
     char *content = readFile(filename);
-    if (content != NULL) {
+    if (content != NULL)
+    {
         printf("\nDateiinhalt:\n%s\n", content);
         free(content); // Speicher freigeben
     }
